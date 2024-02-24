98276723841ef8586f2d121113aa0a162f6c0574
wilson-lin0 <wilsonlinwilin@gmail.com>
2024-02-23 22:22:21 -0500

    Committed a3 to a2 by accident

--- src/Kanbas/Database/index.tsx
+++
@@ -1,5 +0,0 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./assignments.json";
-
-export { courses, modules, assignments };
--- src/Labs/a3/Highlight.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-function Highlight({ children }: { children: ReactNode }) {
-    return (
-        <span style={{ backgroundColor: "yellow", color: "red" }}>
-            {children}
-        </span>
-    );
-}
-export default Highlight;
