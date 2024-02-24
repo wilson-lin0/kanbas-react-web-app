302a315c2b85128261b810338c60388509541a73
wilson-lin0 <wilsonlinwilin@gmail.com>
2024-02-23 22:21:00 -0500

    Course Navigate index.css

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,4 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+
 function CourseNavigation() {
   const links = ["Home", "Modules", "Piazza", "Grades", "Assignments"];
   const { pathname } = useLocation();
